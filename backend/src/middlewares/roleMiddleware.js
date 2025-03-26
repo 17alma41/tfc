da8eee0b86e12448caa2ab6359aadbc02a0d97cc
Alvaro <17alma41@gmail.com>
2025-03-26 17:22:20 +0100

    add: JSDoc

--- backend/src/middlewares/roleMiddleware.js
+++ backend/src/middlewares/roleMiddleware.js
@@ -1,3 +1,10 @@
+/**
+ * Middleware para verificar el rol del usuario.
+ *
+ * @param {string} requiredRole - El rol requerido para acceder al recurso.
+ * @returns {Function} Middleware que verifica si el usuario tiene el rol requerido.
+ */
+
 const roleMiddleware = (requiredRole) => {
     return (req, res, next) => {
         const user = req.user;
